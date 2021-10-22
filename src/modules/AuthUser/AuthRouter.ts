eb9b6f37795c12ee3705427c9878305e46709f68
isFernandes <iago.stosfernandes@gmail.com>
2021-10-21 22:03:31 -0300

    rota de autenticacao criada

---
+++ src/modules/AuthUser/AuthRouter.ts
@@ -0,0 +1,8 @@
+import {Router} from "express";
+import { AuthUserController } from "./controllers/AuthUserController";
+
+const authRouter = Router();
+
+authRouter.post("/authenticate",  new AuthUserController().handle)
+
+export { authRouter }
